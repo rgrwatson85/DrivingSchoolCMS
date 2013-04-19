9836d44040b6186aaa2b00a81fc5507bad402128
Morgan Watson <mrgnwatson@yahoo.com>
2013-04-19 13:17:20 -0500

    Added Main entry point and its controller.

---
+++ src/com/mjwtech/MainController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mjwtech;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author mrgnwatson
+ */
+public class MainController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
